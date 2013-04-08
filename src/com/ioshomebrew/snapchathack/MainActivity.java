61a6521d371850a3edc3c99519e67572a2580ee1
Beecher Adams <koalaos127@gmail.com>
2013-04-08 16:57:45 -0700

    Added a thread method for checking for SnapChat

--- src/com/ioshomebrew/snapchathack/MainActivity.java
+++ src/com/ioshomebrew/snapchathack/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ioshomebrew.snapchathack;
 
+import java.util.Calendar;
 import java.util.List;
 
 import android.os.Bundle;
@@ -9,6 +10,10 @@ import android.os.Messenger;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
@@ -21,6 +26,7 @@ public class MainActivity extends Activity {
 	private Intent intent;
 	private Messenger messenger;
 	static Context context;
+	private boolean threadStarted = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +41,33 @@ public class MainActivity extends Activity {
         intent = new Intent(this, SnapChatService.class);
         messenger = new Messenger(toastTeller);
         context = this;
-		setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 	}
-
+	
+	// app checking thread
+	Thread thread = new Thread()
+	{
+		public void run()
+		{
+			while(threadStarted)
+			{
+				if(checkForSnapChat())
+				{
+					// tell user to start screen recording software
+					showToast("Please start your screen recording software now");
+					
+					// pause for 30 seconds before restarting service
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}	
+			}
+		}
+	};
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -48,18 +78,42 @@ public class MainActivity extends Activity {
 	// start the service
 	public void Start(View view)
 	{
-		intent.putExtra("MESSENGER", messenger);
-		startService(intent);
+		// start the service
+		if(!isMyServiceRunning())
+		{
+			intent.putExtra("MESSENGER", messenger);
+			startService(intent);
+		}
 	}
 	
 	// stop the service
 	public void Stop(View view)
 	{	
 		// stop the service
-		stopService(intent);
+		if(isMyServiceRunning())
+		{
+			stopService(intent);
+			
+			// tell user service was stopped
+			Toast.makeText(this, "Service was stopped", Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	// start the thread
+	public void startThread(View view)
+	{
+		// mark thread started as true
+		threadStarted = true;
 		
-		// tell user service was stopped
-		Toast.makeText(this, "Service was stopped", Toast.LENGTH_LONG).show();
+		// start the thread
+		thread.start();
+	}
+	
+	// stop the thread
+	public void stopThread(View view)
+	{
+		// mark thread started as false
+		threadStarted = false;
 	}
 	
 	// function to check for snapchat
@@ -74,5 +128,26 @@ public class MainActivity extends Activity {
 		}
 		    
 		return false;
+	}
+	
+	// check if SnapChatHack service is running
+	private boolean isMyServiceRunning() {
+	    ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if ("com.example.MyService".equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
+	public void showToast(final String toast)
+	{
+	    runOnUiThread(new Runnable() {
+	        public void run()
+	        {
+	            Toast.makeText(MainActivity.this, toast, Toast.LENGTH_SHORT).show();
+	        }
+	    });
 	}
 }
